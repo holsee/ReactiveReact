ed6db31694b0e2c5aca1160f53b65318aacb599e
Steven Holdsworth <sholdsworth@gmail.com>
2014-04-29 00:49:48 +0100

    init

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  grunt.initConfig({
+    
+    react: {
+      jsx: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/views',
+            src: [ '**/*.jsx' ],
+            dest: 'public/js',
+            ext: '.js'
+          }
+        ]
+      }
+    },
+ 
+    watch: {
+      react: {
+        files: 'public/js/**/*.jsx',
+        tasks: ['react']
+      }
+    }
+  });
+ 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-react');
+  
+  grunt.registerTask('default', ['react']);
+};
