3720bd059f7bf291ef16a8430d9c9da5975557fa
Steven Holdsworth <sholdsworth@gmail.com>
2014-04-29 01:09:56 +0100

    Executing Rx in .jsx

--- src/views/hi.jsx
+++ src/views/hi.jsx
@@ -1,5 +1,10 @@
   /** @jsx React.DOM */
   React.renderComponent(
-    <h1>Hello, world!</h1>,
-    document.getElementById('example')
+    <h1 id='title'>Hello, world!</h1>,
+    document.getElementById('example'),
+    function(){ 
+      Rx.Observable.range(0, 100000).subscribe(function(x) {
+        document.getElementById('title').innerHTML  = x;
+      });
+    }
   );
