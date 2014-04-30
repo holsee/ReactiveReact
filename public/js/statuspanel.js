39c232be1640d2e084f76919abca2593a1a76cb4
Steven Holdsworth <sholdsworth@gmail.com>
2014-04-30 01:40:47 +0100

    Setting data in component from subject (price board)

--- public/js/statuspanel.js
+++ public/js/statuspanel.js
@@ -1,9 +1,23 @@
 /** @jsx React.DOM */
+
+//
+// Components
+
 var StatusPanel = React.createClass({displayName: 'StatusPanel',
+  getInitialState: function(){
+    return {
+      status: 'up',
+      price: 0.0
+    };
+  },
+  onData: function(data){
+    var status = this.state.price < data.price ? 'up' : 'down';
+    this.setState({status: status, price: data.price});
+  },
   render: function() {
     return (
       React.DOM.div( {className:"statusPanel"}, 
-        Status( {status: this.props.status } )
+        Status( {status: this.state.status,  price: this.state.price } )
       )
     );
   }
@@ -12,23 +26,28 @@ var StatusPanel = React.createClass({displayName: 'StatusPanel',
 var Status = React.createClass({displayName: 'Status',
   render: function() {
     return (
-      React.DOM.div( {className: 'status-' + this.props.status } )
+      React.DOM.div( {className: 'status-' + this.props.status } , this.props.price)
     );
   }
 });
 
-var subject = new Rx.Subject();
+var statusPanel = StatusPanel();
+React.renderComponent(
+  statusPanel,
+  document.getElementById('content')
+);
 
+//
+// Streams
+
+// Initially has value open...
+var subject = new Rx.BehaviorSubject();
 
 var source = subject;
-
-var subscription = subject.subscribe(
+var subscription = source.subscribe(
   function (x) {
     console.log('Subject States:', x);
-    React.renderComponent(
-      StatusPanel( { status: x } ),
-      document.getElementById('content')
-    );
+    statusPanel.onData({ price: x });
   },
   function (err) {
     console.log('Error: ', err);   
@@ -36,3 +55,7 @@ var subscription = subject.subscribe(
   function () {
     console.log('Completed');   
   });
+
+// ... later this is changed to matched.
+//subject.onNext('matched'); 
+//subject.onCompleted();
